5142bbde90c07bfcb67351402969c929111a212e
Juanmaferreyra <juan_manuelferreyra@hotmail.com>
2026-03-07 19:38:52 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'fincontrol-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación: cachea los archivos principales
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activación: limpia cachés viejos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: sirve desde caché, si no hay va a la red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        // Cachea nuevos recursos dinámicamente
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
